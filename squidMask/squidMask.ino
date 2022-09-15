8bfff7d6777589eb83bb8673db1ec37ab991bdaf
Gabriel <gabrieljones2001@outlook.com>
2022-09-15 08:40:16 -0700

    Rainbow complete
    I got the rainbow effect done for the mask. I still have to define how many leds are in the string, but other then that I should be on my way to completing most of the mask. I still need to code the main glitch effect that will make this all come together. I will be updating that in two parts do to the fact that I currently do not have the full string put together. -GJ

--- squidMask/squidMask.ino
+++ squidMask/squidMask.ino
@@ -3,8 +3,9 @@
 //all values are going to be usefull later to define
 //where the leds are going to be controlled
 //may be an idea later, but maybe add different themes
-#define ledPin A1
+#define ledPin A4
 #define ledCount 100
+#define white strip.Color(255,255,255)
 
 Adafruit_NeoPixel strip(ledCount, ledPin, NEO_GRB + NEO_KHZ800);//you may need to change to NEO_RGB
 
@@ -13,8 +14,9 @@ Adafruit_NeoPixel strip(ledCount, ledPin, NEO_GRB + NEO_KHZ800);//you may need t
 void setup() {
 
   strip.begin();
-  strip.setBrightness(100);
-  trianglefunc();
+  strip.setBrightness(25);
+  delay(500);
+  rainbow();
 
 }
 
@@ -24,27 +26,78 @@ void loop() {
   // method.show() updates method
   // method.setBrightness self explanitory
 
-  //pre-defining possible ports for a big if else statment
-  int triangle = A0;
-  int rainbow = A1;
-
-  if(digitalRead(rainbow) == HIGH){
+  /*if(digitalRead(2) == HIGH){
     //initial;
     strip.clear();
-    //rainbowfunc();
+    rainbow();
   }
-  if(digitalRead(triangle) == HIGH) {
+  if(digitalRead(3) == HIGH) {
     //initial;
     strip.clear();
-    trianglefunc();
-  }
+    basic();
+  }*/
 
 }
 
-void trianglefunc() {
+void basic() {
   //this is going to be the glitch setup and will run upon startup
   //initial
-  strip.fill(strip.Color(255,255,255,0),0,100); //color, starting led, ending led
+  strip.fill(white); //color, starting led, ending led
   strip.show();
   
 }
+
+
+void rainbow() {
+  int c = 0;
+  int i = 0;
+  int x = 0;
+  int y = 0;
+  int z = 0;
+
+  while(z < 255){
+    strip.fill(strip.Color(x,y,z),0,100);
+    strip.show();
+    delay(10);
+    z++;
+  }
+
+  while(c == 0){
+    while(i == 0){
+      strip.fill(strip.Color(x,y,z),0,100);
+      strip.show();
+      delay(10);
+      z--;
+      x++;
+      if(x == 255){
+        i++;
+      }
+    }
+
+    while(i == 1){
+      strip.fill(strip.Color(x,y,z),0,100);
+      strip.show();
+      delay(10);
+      x--;
+      y++;
+      if(y == 255){
+        i++;
+      }
+    }
+
+    while(i == 2){
+      strip.fill(strip.Color(x,y,z),0,100);
+      strip.show();
+      delay(10);
+      y--;
+      z++;
+      if(z == 255){
+        i = 0;
+      }
+    }
+  }
+}
+
+
+
+
